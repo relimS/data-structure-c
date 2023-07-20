97725025eb2c3ab61e69978a9ece187b29e96d24
relimS <relims@relims.one>
2023-07-20 21:42:05 +0700

    message

--- hashtable/hashtable.h
+++ hashtable/hashtable.h
@@ -19,6 +19,7 @@ typedef struct Hash_table {
 
 extern int ht_init(Hash_table *table, size_t n);
 extern Hash_bucket *ht_add(Hash_table *table, const void *key, size_t key_size, const void *value, size_t value_size, size_t, int (*)(const void *, const void *));
+extern Hash_bucket *ht_add_fast(Hash_table *table, const void *key, size_t key_size, const void *value, size_t value_size, size_t hash);
 extern void __ht_bucket_free_recursive(Hash_bucket *table);
 extern Hash_bucket *ht_lookup(Hash_table *table, const void *key, size_t size, int (*)(const void *, const void *));
 extern Hash_bucket_location ht_lookup_location(Hash_table *table, const void *key, size_t hash, int (*)(const void *, const void *));
--- hashtable/test.c
+++ hashtable/test.c
@@ -2,17 +2,22 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <sys/random.h>
 #include <time.h>
 #include "hashtable.h"
 
 #define FNV_offset_basis 14695981039346656037U
 #define FNV_prime 1099511628211U
 
-#define n 1000000
-#define n_str "1000000"
-#define ten_n_str "10000000"
+
+#define STRINGIZE_(x) #x
+#define STRINGIZE(x) STRINGIZE_(x)
+
+#define N 67108864
+#define TABLE_N 67108864
+
+#define N_STR STRINGIZE(N)
+#define TABLE_N_STR STRINGIZE(TABLE_N)
+
 
 extern size_t fnv1a(const void *data, size_t bytes) {
     size_t i, Hash;
@@ -37,45 +42,60 @@ int main(void) {
     bool ranoutofidea;
     Hash_bucket_location lot;
     clock_t start, end;
-    keys = (size_t*)malloc(n*sizeof(size_t));
-    vals = (size_t*)malloc(n*sizeof(size_t));
-    if (!keys || !vals) {
+    keys = (size_t*)malloc(N*sizeof(size_t));
+    if (!keys) {
         fprintf(stderr, "Cannot allocate memory!\n");
         return 1;
     }
-    fputs("filling "n_str" keys and "n_str" values arrays up with random data... ", stdout);
+    vals = (size_t*)malloc(N*sizeof(size_t));
+    if (!vals) {
+        free(keys);
+        fprintf(stderr, "Cannot allocate memory!\n");
+        return 1;
+    }
+    fputs("filling "N_STR" keys and "N_STR" values arrays up with random data... ", stdout);
     fflush(stdout);
     start = clock();
-    getrandom(vals, n*sizeof(*vals), 0);
-    getrandom(keys, n*sizeof(*keys), 0);
-    qsort(keys, n, sizeof(*keys), compar);
+    srand(time(NULL));
+    for (i = 0; i != N*sizeof(size_t)/sizeof(int); ++i) {
+        ((int*)keys)[i] = rand();
+        ((int*)vals)[i] = rand();
+    }
+    qsort(keys, N, sizeof(*keys), compar);
     ranoutofidea = true;
     while (ranoutofidea) {
         ranoutofidea = false;
-        for (i = 0; i != n-1; ++i) {
-            if (keys[i] == keys[i+1]) {
-                ranoutofidea = true;
-                getrandom(keys+i, sizeof(*keys), 0);
-                for (++i; i != n-1; ++i) {
-                    if (keys[i] == keys[i+1]) getrandom(keys+i, sizeof(*keys), 0);
-                }
-                qsort(keys, n, sizeof(*keys), compar);
-                break;
+        for (i = 0; i != N-1; ++i) {
+            if (keys[i] != keys[i+1]) {
+                continue;
             }
+            ranoutofidea = true;
+            for (j = 0; j != sizeof(size_t)/sizeof(int); ++j) ((int*)keys)[j] = rand();
+            for (++i; i != N-1; ++i) {
+                if (keys[i] == keys[i+1]) for (j = 0; j != sizeof(size_t)/sizeof(int); ++j) ((int*)keys)[j] = rand();
+            }
+            qsort(keys, N, sizeof(*keys), compar);
+            break;
         }
     }
     end = clock();
-    printf("done. took %fs.\ninitializing the hash table of "ten_n_str"... ", (float)(end-start)/CLOCKS_PER_SEC);
+    printf("done. took %fs.\ninitializing the hash table of "TABLE_N_STR"... ", (float)(end-start)/CLOCKS_PER_SEC);
     fflush(stdout);
     start = clock();
-    ht_init(&table, n*10);
+    ht_init(&table, TABLE_N);
     end = clock();
     fputs("done.\nfilling the hash table with the keys and values... ", stdout);
     fflush(stdout);
     start = clock();
-    buckets = (Hash_bucket**)malloc(n*sizeof(Hash_bucket*));
-    for (i = 0; i != n; ++i) {
-        bucket = ht_add(&table, keys+i, sizeof(size_t), vals+i, sizeof(size_t), fnv1a(keys+i, sizeof(size_t)), compar);
+    buckets = (Hash_bucket**)malloc(N*sizeof(Hash_bucket*));
+    if(!buckets) {
+        free(keys);
+        free(vals);
+        fprintf(stderr, "Cannot allocate memory!\n");
+        return 1;
+    }
+    for (i = 0; i != N; ++i) {
+        bucket = ht_add_fast(&table, keys+i, sizeof(size_t), vals+i, sizeof(size_t), fnv1a(keys+i, sizeof(size_t)));
         if (!bucket || compar(keys+i, bucket->key) || compar(vals+i, bucket->value)) {
             printf("failed\n");
             break;
@@ -86,7 +106,7 @@ int main(void) {
     printf("done. took %fs.\nreading data from the hash table ... ", (float)(end-start)/CLOCKS_PER_SEC);
     fflush(stdout);
     start = clock();
-    for (i = 0; i != n; ++i) {
+    for (i = 0; i != N; ++i) {
         bucket = ht_lookup(&table, keys+i, fnv1a(keys+i, sizeof(size_t)), compar);
         if (!bucket) {
             fprintf(stderr, "failed\n");
@@ -99,8 +119,11 @@ int main(void) {
     end = clock();
     free(buckets);
     printf("done. took %fs.\nremoving a bucket ... ", (float)(end-start)/CLOCKS_PER_SEC);
-    j = 0;
-    while ((j %= n) == 0) getrandom(&j, sizeof(j), 0);
+    i = 0;
+    while (!(i%=N)) {
+        for (j = 0; j != sizeof(size_t)/sizeof(int); ++j) ((int*)&i)[j] = rand();
+    }
+
     lot = ht_lookup_location(&table, keys+j, fnv1a(keys+j, sizeof(size_t)), compar);
     if (!lot.depth) printf("failed\n");
     else {
